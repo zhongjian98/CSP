c905f14d40d60e41e7e2e049f44bc265af39e907
zj <599674166@qq.com>
2021-04-06 17:49:30 +0800

    2021年4月6日17:49:19 修改上传

--- src/day19/test2.java
+++ src/day19/test2.java
@@ -11,6 +11,7 @@ public class test2 {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         int a[] = new int[505];
+        int ww=0;
         for (int i = 0; i < n; i++) {
             a[i] = scanner.nextInt();
         }
